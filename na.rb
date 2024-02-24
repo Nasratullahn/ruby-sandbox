38fb706dcb96f3f546969f1e7b2b31ec302e3677
Nasratullah Nasrat <60459439+Nasratullahn@users.noreply.github.com>
2024-02-24 21:32:17 +0000

    ge

--- na.rb
+++ na.rb
@@ -1,3 +1,24 @@
 require "active_support/all"
 
-pp "hello"
+class Car
+  attr_reader :year
+
+  def initialize(year)
+    @year = year
+  end
+  
+  def year=(value)
+    unless value.is_a? Integer
+      raise TypeError, 'Year must be a integer'
+    end
+    @year = value
+  end
+end
+
+
+# Test your Car class
+car = Car.new(2021)
+pp car.year
+# Change and then print the year
+car.year = 2022
+pp car.year
